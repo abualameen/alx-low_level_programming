cadf67c26a7190c5d06f7af993344609f2f78668
abu al ameen <abulyaqs@gmail.com>
2023-03-17 20:43:14 +0100

    printing of alphabet using decimal code and putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printing of alpha char to std output'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int p = 97;
+	
+	while (p <= 122)
+	{
+		putchar(p);
+		p++;
+	}
+	putchar('\n');
+	return (0);
+}
