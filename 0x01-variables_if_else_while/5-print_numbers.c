40fa45c131662f825666006eeba8e604f9471a8e
abu al ameen <abulyaqs@gmail.com>
2023-03-17 21:52:20 +0100

    print numbs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Description: 'print num xero to nine'
+ * Return: always 0
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 0; g <= 9; g++)
+	{
+		printf("%d", g);
+	}
+	printf("\n");
+	return (0);
+}
