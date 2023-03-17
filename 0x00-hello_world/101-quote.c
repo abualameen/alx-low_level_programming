42ea2cb41259826d85ff4e9ecb793814ba5ff3a7
abualmashkur <abulyaqs@gmail.com>
2023-03-16 18:51:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A programe in uniz for error
+ * Return: 1 if (seccess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
