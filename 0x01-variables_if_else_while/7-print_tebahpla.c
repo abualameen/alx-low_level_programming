a918e80a4620c4ece5049d6c166fec9f93967959
abu al ameen <abulyaqs@gmail.com>
2023-03-17 22:17:01 +0100

    backwards alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'alhabet in reverse form'
+ * Return: always 0
+ */
+int main(void)
+{
+	int t = 122;
+	
+	while (t >= 97)
+	{
+		putchar(t);
+		t--;
+	}
+	putchar('\n');
+	return (0);
+}
