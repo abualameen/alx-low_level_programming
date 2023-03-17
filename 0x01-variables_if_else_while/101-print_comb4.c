58ee07d74cf384345c03e5feb620181d9ff4d437
abu al ameen <abulyaqs@gmail.com>
2023-03-18 00:51:46 +0100

    cmb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printing three digits'
+ * Return: always 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int p;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 1; n < 10; n++)
+		{
+			for (p = 2; p < 10; p++)
+			{
+				if (m < n && m != n && n != p && m != p)
+				{
+					putchar(m + '0');
+					putchar(n + '0');
+					putchar(p + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
