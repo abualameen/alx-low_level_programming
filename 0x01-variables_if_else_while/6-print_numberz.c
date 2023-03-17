4bf8fe27c0571ae0edfd8382010a5d31e4a2cf8b
abu al ameen <abulyaqs@gmail.com>
2023-03-17 22:05:15 +0100

    print numb using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main entry in cod'
+ * Description: 'to print numbers with charput'
+ * Return: always 0
+ */
+int main(void)
+{
+	int e = 48;
+
+	while (e <= 57)
+	{
+		putchar(e);
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
+
