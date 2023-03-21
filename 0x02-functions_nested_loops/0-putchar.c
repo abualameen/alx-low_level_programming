dbf849e07aa30d47a1efd9c33a501c97b1b133a4
abu al ameen <abulyaqs@gmail.com>
2023-03-21 15:07:27 +0100

    using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point to code
+ * Description: ' this is a prog to print'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
