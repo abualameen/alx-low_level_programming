df17d79a454c558f631b05b539f7bfd3229f2f4c
abu al ameen <abulyaqs@gmail.com>
2023-03-21 18:33:53 +0100

    alphabet with funtion

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *   main - Entry point to code
+ *   Description: ' print alphabet'
+ *   Return: always 0
+ */
+void print_alphabet(void)
+{
+	int j;
+	
+	for (j = 97; j <= 122; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
