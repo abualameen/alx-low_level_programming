9431acf2cbb079351c41e295b28322386d8a9160
abu al ameen <abulyaqs@gmail.com>
2023-03-22 22:13:00 +0100

    add two numb

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - to add
+ * @a: 'parameter of function'
+ * @b: 'parameter for function'
+ * Return: c
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
