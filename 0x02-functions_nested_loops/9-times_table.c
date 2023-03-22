0d7d5810e1079b29b90d06f41f58cac672be5d76
abu al ameen <abulyaqs@gmail.com>
2023-03-22 21:23:11 +0100

    nine times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,48 +1,39 @@
 #include "main.h"
 /**
- * times_table - nine times table
- * Return: void
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-	int m, a, n, p, q;
+	int m, n, a, q;
 
 	for (m = 0; m <= 9; m++)
 	{
 		for (n = 0; n < 10; n++)
 		{
 			a = m * (n);
-			if (a <= 9)
+			if (n == 0)
 			{
 				_putchar(a + '0');
-				if (n == 9)
-				{
-					continue;
-				}
+			}
+			if (a <= 9 && n != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(a + '0');
 			}
-			else
+			else if (a > 9)
 			{
 				q = a % 10;
 				while (a >= 10)
 				{
 					a /= 10;
 				}
-				_putchar(a + '0');
-				_putchar( q + '0');
-				if (n == 9)
-				{
-					continue;
-				}
 				_putchar(',');
 				_putchar(' ');
+				_putchar(a + '0');
+				_putchar(q + '0');
 			}
-			if (m == 2 && n == 4 || m == 3 && n == 3 || m == 4 && n == 2 || m == 5 && n == 1 || m == 6 && n == 1 || m == 7 && n == 1 || m == 8 && n == 1 || m == 9 && n == 1)
-		{
-			_putchar('\b');
-		}
 		}
 		_putchar('\n');
 	}
