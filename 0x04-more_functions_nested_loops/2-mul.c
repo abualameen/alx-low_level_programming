52921f5ec61409c96975f9d73de376ea7c6a51f9
abu al ameen <abulyaqs@gmail.com>
2023-03-24 12:50:00 +0100

    multi two numbs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - this function multi two numb
+ * @a: funciton argument
+ * @b: function argument
+ * Return: always c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
