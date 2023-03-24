509bacb76d57ce0a6702cb6a6f6390336238f11d
abu al ameen <abulyaqs@gmail.com>
2023-03-24 15:33:11 +0100

    nub without two and four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,18 @@
  * print_most_numbers - function that prints numb
  * Return: always 0
  */
-
+	
 void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c <= 57; c++)
+ 	for (c = 48; c <= 57; c++)
 	{
-		if (c != 2 || c != 4)
+ 		if (c == 50 || c == 52)
 		{
-			_putchar(c);
+			continue;
 		}
+		_putchar(c);
 	}
 	_putchar('\n');
 }
