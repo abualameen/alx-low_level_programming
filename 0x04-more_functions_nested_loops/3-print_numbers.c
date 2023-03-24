435e8f3e736075bfa9e57dfeea1f753d94cc503e
abu al ameen <abulyaqs@gmail.com>
2023-03-24 13:16:10 +0100

    print numbs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numb
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
