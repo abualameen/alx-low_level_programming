c4e9948bb77c565d04722e3616d1323288b4b588
abu al ameen <abulyaqs@gmail.com>
2023-03-27 22:49:26 +0100

    revesed str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev -  print str in reverse
+ * @s: parameters of the function
+ */
+
+void print_rev(char *s)
+{
+	int g;
+	int count;
+	int f;
+
+	for (g = 0; s[g] != '\0'; g++)
+	{
+		count++;
+	}
+	for (f = count; f >= 0; f--)
+	{
+		_putchar(s[f]);
+	}
+	_putchar('\n');
+}
