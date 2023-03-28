9f6b8b9bb0fdbe7134fc89aefaede007d1c28193
abu al ameen <abulyaqs@gmail.com>
2023-03-28 11:34:59 +0100

    rev str two

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,20 @@
 
 void rev_string(char *s)
 {
-        char ica = s[0];
-        int d = 0;
-	int count = 0;
+        char xca = s[0];
+	int d;
+	int count = 0; 
         int p;
 
-        for (d = 0; d != '\0'; d++)
+        for (d = 0; s[d] != '\0'; d++)
         {
                 count++;
         }
         for (p = 0; p < count; p++)
-        {
+     	{
                 count--;
-                ica = s[p];
+                xca = s[p];
                 s[p] = s[count];
-                s[count] = ica;
+                s[count] = xca;
         }
 }
