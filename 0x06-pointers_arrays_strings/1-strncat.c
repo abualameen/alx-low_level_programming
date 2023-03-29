031422638ac72b7f74d8b608d97c123ff0b3d2b3
abu al ameen <abulyaqs@gmail.com>
2023-03-29 21:28:12 +0100

    string conca n numb

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - the function concatenante n len of strings
+ * @dest: the destination location string
+ * @src: the source string
+ * @n: function argument
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int g;
+	int count = 0;
+	int f;
+
+	for (g = 0; dest[g] != '\0'; g++)
+	{
+		count++;
+	}
+	for (f = 0; f < n; f++)
+	{
+		dest[count + f] = src[f];
+	}
+	dest[count + f] = '\0';
+	return (dest);
+}
