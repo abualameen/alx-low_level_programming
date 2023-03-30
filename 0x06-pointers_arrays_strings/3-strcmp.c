45aa2aa7f3249c8d6b547efdb38c6f79723322ba
abu al ameen <abulyaqs@gmail.com>
2023-03-30 16:19:40 +0100

    comapares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,40 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if ((s1[1] - s2[1]) == 0)
+	if ((s1[0] - s2[0]) == 0)
 	{
-		return (s1[1] - s2[1]);
+		return (s1[0] - s2[0]);
+	} 
+	else if ((s1[0] - s2[0]) < 0)
+	{
+		return (s1[0] - s2[0]);
 	}
-	else if ((s1[1] - s2[1]) < 0)
+	else if ((s1[0] - s2[0]) > 0)
 	{
-		return (s1[1] - s2[1]);
+		return (s1[0] - s2[0]);
 	}
-	else if ((s1[1] - s2[1]) > 0)
+	else if (s2[0] - s1[0] == 0)
+ 	{
+		return (s2[0] - s1[0]);
+	}
+	else if (s2[0] - s1[0] < 0)
 	{
-		return (s1[1] - s2[1]);
+		return (s2[0] - s1[0]);
+	}
+	else if (s2[0] - s1[0] > 0)
+	{
+		return (s2[0] - s1[0]);
+	}
+	else if (s1[0] - s1[0] == 0)
+	{
+		return (s1[0] - s1[0]);
+	}
+        else if (s1[0] - s1[0] < 0)
+	{
+		return (s1[0] - s1[0]);
+	}
+	else if (s1[0] - s1[0] > 0)
+	{
+		return (s1[0] - s1[0]);
 	}
 }
