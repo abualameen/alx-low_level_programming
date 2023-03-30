99e4765b50249435eb630b3c71560805f1c47d25
abu al ameen <abulyaqs@gmail.com>
2023-03-30 15:52:11 +0100

    comapares strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1[0] - s2[0] == 0)
+	if ((s1[0] - s2[0]) == 0)
 	{
 		return (s1[0] - s2[0]);
 	}
-	else if (s1[0] - s2[0] < 0)
+	else if ((s1[0] - s2[0]) < 0)
 	{
 		return (s1[0] - s2[0]);
 	}
-	else if (s1[0] - s2[0] > 0)
+	else if ((s1[0] - s2[0]) > 0)
 	{
 		return (s1[0] - s2[0]);
 	}
