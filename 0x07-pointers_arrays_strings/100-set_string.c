961795f1f8390036646787c45f5cf49fa5093cfb
abu al ameen <abulyaqs@gmail.com>
2023-04-04 22:35:03 +0100

    seting point to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function set pointer to char
+ * @s: funttion argument p-p
+ * @to: function argumern
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
