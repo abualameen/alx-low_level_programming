5000451196f265747f2377d8fc656ea69bcfc8cc
abu al ameen <abulyaqs@gmail.com>
2023-04-08 13:33:59 +0100

    palindrome recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "main.h"
+int main_is_palindrome(char *s, int start, int end);
+int str_count(char *s);
+
+/**
+ * is_palindrome - this funntion determines if a str is palindrmn
+ * @s: argument
+ * Return: 0
+ */
+
+int is_palindrome(char *s)
+{
+	return (main_is_palindrome(s, 0, str_count(s) - 1));
+}
+
+/**
+ * str_count - count the len of a str
+ * @s: function argu
+ * Return: count
+ */
+
+int str_count(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	count++;
+	}
+	return (count);
+}
+
+
+/**
+ * main_is_palindrome - called recuresive function
+ * @s: function argu
+ * @start: arugu
+ * @end: argu
+ * Return: 0
+ */
+
+int main_is_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] == s[end])
+	{
+		return (main_is_palindrome(s, start + 1, end - 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
