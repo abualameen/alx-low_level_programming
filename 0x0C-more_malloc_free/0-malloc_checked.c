123f055dcbd25bb91574c5ef8ad342d62223261d
abu al ameen <abulyaqs@gmail.com>
2023-04-13 11:39:31 +0100

    malloc space allo

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - this function allocates memory using malloc
+ * @b: function argument representing the amount of memery
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
