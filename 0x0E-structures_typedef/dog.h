7d0e21d92b7f57c575c5a2df3482a274369b064f
abu al ameen <abulyaqs@gmail.com>
2023-04-17 18:50:19 +0100

    starting with struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+/**
+ * struct dog - tthe strut called dog
+ * Description: The function creat an instance of a dog
+ * @name: attribute
+ * @age: attribute
+ * @owner: attribute
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
