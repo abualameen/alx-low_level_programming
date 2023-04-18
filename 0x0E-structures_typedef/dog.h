2a7fba8efb6fdded0374498b1febedeadaa9a90b
abu al ameen <abulyaqs@gmail.com>
2023-04-18 15:20:02 +0100

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,20 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - tthe strut called dog
+ * struct dog - the strut called dog
  * Description: The function creat an instance of a dog
  * @name: attribute
  * @age: attribute
  * @owner: attribute
  * Return: 0
  */
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
 
 
 #endif
