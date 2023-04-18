020f9cdb5e76af98fdf18b6f512ea116662a9a3b
abu al ameen <abulyaqs@gmail.com>
2023-04-18 14:43:02 +0100

    print the dog atrr

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,19 +15,13 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).name == NULL)
 	{
-		printf("(nil)");
+		(*d).name = "(nil)";
 	}
-	else
-	{
-		printf("Name: %s\n", (*d).name);
-	}
+	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	if ((*d).owner == NULL)
 	{
-		printf("(nil)");
+		(*d).owner = "(nil)";
 	}
-	else
-	{
-		printf("Owner: %s\n", (*d).owner);
-	}
+	printf("Owner: %s\n", (*d).owner);
 }
