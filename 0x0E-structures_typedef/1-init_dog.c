cd954f73624663da3e0c4a897d29dc4ef2895976
abu al ameen <abulyaqs@gmail.com>
2023-04-18 11:34:21 +0100

    function that initiates a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - function declearation
+ * @d: argument
+ * @name: argument
+ * @age: argument
+ * @owner: argument
+ * Return: initiated dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
