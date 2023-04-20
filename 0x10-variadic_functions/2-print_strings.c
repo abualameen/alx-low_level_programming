bc802dcea914b9610c8c5b528302d08c44dd8c5a
abu al ameen <abulyaqs@gmail.com>
2023-04-20 18:37:30 +0100

    print string with va list

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", r);
 		}
-		if (t < n - 1 && separator != NULL)
+		if (t != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
