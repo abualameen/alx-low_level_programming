96dae5552159f706ce687589be44e245b00bde8f
abu al ameen <abulyaqs@gmail.com>
2023-04-20 18:17:52 +0100

    print string with va list

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - print strings
+ * @separator: space puter
+ * @n: number of str
+ * Return: 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int t;
+	char *r;
+
+	va_start(args, n);
+	for (t = 0; t < n; t++)
+	{
+		r = va_arg(args, char *);
+		printf("%s", r);
+		if (t < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
