e4ed5e1afb25c6d61e1e5f6d0a5f2ed3cf3c2008
abu al ameen <abulyaqs@gmail.com>
2023-04-20 09:23:29 +0100

    first full prograrmme i wrote on ALX SE

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,10 +11,7 @@
 
 int op_add(int a, int b)
 {
-	int t;
-
-	t = a + b;
-	return (t);
+	return (a + b);
 }
 
 /**
@@ -26,10 +23,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-	int t;
-
-	t = a - b;
-	return (t);
+	return (a - b);
 }
 
 /**
@@ -41,10 +35,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	int t;
-
-	t = a * b;
-	return (t);
+	return (a * b);
 }
 
 /**
@@ -56,10 +47,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	int t;
-
-	t = a / b;
-	return (t);
+	return (a / b);
 }
 
 /**
@@ -71,8 +59,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	int t;
-
-	t = a % b;
-	return (t);
+	return (a % b);
 }
