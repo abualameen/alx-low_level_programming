82532bd3fdcca4f769293616113802851b0a02c9
abu al ameen <abulyaqs@gmail.com>
2023-04-20 09:10:41 +0100

    first full prograrmme i wrote on ALX SE

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,6 +15,11 @@ int main(int argc, char *argv[])
 	int (*ptr_func)(int, int);
 	(void)argc;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2];
