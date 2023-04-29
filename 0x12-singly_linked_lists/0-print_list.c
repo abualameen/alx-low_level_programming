49636777ec25870f078de3ed878678fe93816aff
abu al ameen <abulyaqs@gmail.com>
2023-04-29 14:20:21 +0100

    prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,10 @@ size_t print_list(const list_t *h)
 {
 	const list_t *latest  = h;
 	size_t counter = 0;
-
+	if (latest == NULL)
+	{
+		printf("[0] (nill)\n");
+	}
 	while (latest)
 	{
 		if ((*latest).str != NULL)
