59eecb727251f33c995ca0d686a9e04cf31f0596
abu al ameen <abulyaqs@gmail.com>
2023-04-29 18:50:17 +0100

    64-bit program in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+section .data
+    hello db "Hello, Holberton", 10, 0
+    format db "%s", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [hello]
+    mov rsi, format
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
+
