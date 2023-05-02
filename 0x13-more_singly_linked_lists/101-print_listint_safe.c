60da3908e5189da24fd3a8d9b41f4e2019dcfacd
abu al ameen <abulyaqs@gmail.com>
2023-05-02 21:31:58 +0100

    handling loop in linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,7 +17,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (floyd_algo(head))
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
+			exit(98);
 		}
 		head = head->next;
 	}
