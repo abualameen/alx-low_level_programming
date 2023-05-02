6f02eb2dfaeeedbd1054da05e5067c4a8d097910
abu al ameen <abulyaqs@gmail.com>
2023-05-02 11:05:46 +0100

    handling loop in linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints linked list struc
+ * @head: pointer to the head node
+ * Return: 0
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *store_node;
+	size_t counter = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, (*head).n);
+		counter++;
+		store_node = head;
+		head = (*head).next;
+		if (store_node <= head)
+		{
+			printf("-> [%p] %d\n", (void *)head, (*head).n);
+			exit(98);
+		}
+	}
+	return (counter);
+
+}
