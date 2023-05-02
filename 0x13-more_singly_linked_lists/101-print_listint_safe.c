602edfd312f0171eaaebdce171fac8288f9f8baa
abu al ameen <abulyaqs@gmail.com>
2023-05-02 21:27:06 +0100

    handling loop in linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+size_t floyd_algo(const listint_t *head);
 /**
  * print_listint_safe - prints linked list struc
  * @head: pointer to the head node
@@ -7,20 +8,44 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *store_node;
 	size_t counter = 0;
 
-	while (head)
+	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, (*head).n);
 		counter++;
-		store_node = head;
-		head = (*head).next;
-		if (store_node <= head)
+		if (floyd_algo(head))
 		{
-			printf("-> [%p] %d\n", (void *)head, (*head).n);
-			exit(98);
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
 		}
+		head = head->next;
 	}
 	return (counter);
+}
+
+
+/**
+ * floyd_algo - detects if there is loop in linked list
+ * @head: pointer to the head node
+ * Return: 0
+ */
+
+size_t floyd_algo(const listint_t *head)
+{
+	const listint_t *slow;
+	const listint_t *fast;
+
+	slow = head;
+	fast = head;
+	while (fast != NULL && (*fast).next != NULL)
+	{
+		slow = (*slow).next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
