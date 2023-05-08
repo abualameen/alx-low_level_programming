1a4607e0284cc94cc455e9b6c4d57d060cc554fa
abu al ameen <abulyaqs@gmail.com>
2023-05-08 19:53:40 +0100

    reads a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * read_textfile - reads and prints file
+ * @filename: name of the file to be read
+ * @letters:  the number of letters it should read and print
+ * Return: letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp;
+	char *buffer;
+	ssize_t num_bytes_r;
+	ssize_t num_bytes_w;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	fp = open("filename", O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
+	buffer = (char *) malloc(letters);
+	if (!buffer)
+	{
+		close(fp);
+		return (0);
+	}
+	num_bytes_r = read(fp, buffer, letters);
+	printf("%ld", num_bytes_r);
+	if (num_bytes_r == -1)
+	{
+		free(buffer);
+		close(fp);
+		return (0);
+	}
+	num_bytes_w = write(STDOUT_FILENO, buffer, num_bytes_r);
+	printf("%ld", num_bytes_w);
+	if (num_bytes_w == -1 || num_bytes_w != num_bytes_r)
+	{
+		free(buffer);
+		close(fp);
+		return (0);
+	}
+	free(buffer);
+	close(fp);
+	return num_bytes_w;
+}
