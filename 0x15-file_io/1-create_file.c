6fbab8f1b1ec64e91d9caf284e875e8121fe902a
abu al ameen <abulyaqs@gmail.com>
2023-05-08 21:44:57 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - create file
+ * @filename:  filename
+ * @text_content: content of the file
+ * Return: 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int num;
+	int t;
+	int counter = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	for (t = 0; text_content[t] != '\0'; t++)
+	{
+		counter++;
+	}
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	num = write(fp, text_content, counter);
+	if (fp == -1)
+	{
+		return (-1);
+	}
+	else if (num == -1)
+	{
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
