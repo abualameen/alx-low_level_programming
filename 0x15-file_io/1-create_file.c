3723a91901504d0111d9ab6f8ce0094e8711ecdd
abu al ameen <abulyaqs@gmail.com>
2023-05-09 12:42:33 +0100

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (!text_content)
+	{
+		return (-1);
+	}
 	for (t = 0; text_content[t] != '\0'; t++)
 	{
 		counter++;
