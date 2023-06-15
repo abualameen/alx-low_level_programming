22c684c554dfb4838fc6f3c3c536b02ffa77f780
abu al ameen <abulyaqs@gmail.com>
2023-06-15 19:46:23 +0100

    print node val of doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_dlistint - the func print d val of the nodes in a doubly linked list
+ * @h: pointer to the head node
+ * Return: data in the nodes
+ */
+
+
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *latest = h;
+	int counter = 0;
+
+	while (latest != NULL)
+	{
+		printf("%d \n", latest->n);
+		counter++;
+		latest = latest->next;
+	}
+	return (counter);
+}
