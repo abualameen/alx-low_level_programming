1feaaaa87da025a65b6a2376877671558fbe88be
abu al ameen <abulyaqs@gmail.com>
2023-06-16 11:19:50 +0100

    add new node to dlinkded list at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - the fucntion add nod to a dlinkedlist
+ * @head: head of the dlinkedlist
+ * @n: val of the head node
+ * Return: 0
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *tail;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head != NULL)
+	{
+		tail = *head;
+		while (tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		tail->next = new_node;
+		new_node->prev = tail;
+	}
+	else if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*head = new_node;
+	}
+	return (new_node);
+}
