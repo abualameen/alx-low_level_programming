23041cc8b8e984cb4eeb4c340f976be91bea724f
abu al ameen <abulyaqs@gmail.com>
2023-06-16 17:06:52 +0100

    get node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,12 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int counter = 0;
 	dlistint_t *latest;
 
-	latest = *h;
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (h == NULL)
 	{
 		return (NULL);
 	}
+	latest = *h;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 	if (idx == 0)
 	{
 		new_node->next = *h;
