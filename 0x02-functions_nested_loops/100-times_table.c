c563bd020ecc8f7131f08f93bdacee125c679ab0
abu al ameen <abulyaqs@gmail.com>
2024-03-10 18:48:17 +0100

    newtassk

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,37 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table, starting with 0.
- * @n: The number of times table to print (0 <= n <= 15).
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times table to print
+ *
+ * Description: If n is greater than 15 or less than 0, the function
+ *              does not print anything.
  */
 void print_times_table(int n)
 {
-    int row, column, product;
+	int i, j, result;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (row = 0; row <= n; row++)
-    {
-        for (column = 0; column <= n; column++)
-        {
-            product = row * column;
-            if (column == 0)
-                _putchar('0');
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10)
-                    _putchar(' ');
-                if (product < 100)
-                    _putchar(' ');
-            }
-            if (product < 10)
-                _putchar(' ');
-            else if (product >= 100)
-            {
-                _putchar((product / 100) + '0');
-                _putchar(((product / 10)) % 10 + '0');
-            }
-            else
-                _putchar((product / 10) + '0');
-            _putchar((product % 10) + '0');
-        }
-        _putchar('\n');
-    }
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0'); /* Print the first column */
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(',');
+			result = i * j;
+			if (result < 100)
+				_putchar(' ');
+			if (result < 10)
+				_putchar(' ');
+			if (result >= 100)
+				_putchar((result / 100) + '0');
+			if (result >= 10)
+				_putchar(((result / 10) % 10) + '0');
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
