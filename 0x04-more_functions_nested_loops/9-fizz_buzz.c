0c5d94326f6c8f5d25fb654c204fb4f855abdc6c
abu al ameen <abulyaqs@gmail.com>
2024-03-10 20:02:01 +0100

    correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,15 @@
 #include "main.h"
-/**
- * main - this is the entry point
- * Description: function that print multiple
- * Return: always 0
- */
 
+/**
+ * main - Entry point
+ *
+ * Description: Function that prints "Fizz" for multiples of 3,
+ *              "Buzz" for multiples of 5, and "FizzBuzz" for
+ *              multiples of both 3 and 5, otherwise, it prints
+ *              the number itself, from 1 to 100
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int m;
