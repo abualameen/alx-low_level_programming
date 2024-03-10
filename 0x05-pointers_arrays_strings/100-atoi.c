b51893cea0db9c93128965203aadab0ca55c995f
abu al ameen <abulyaqs@gmail.com>
2024-03-10 21:19:58 +0100

    correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,45 @@
 #include "main.h"
-/**
- * _atoi - this function conv str to int
- * @s: function argument
- * Return: always 0
- */
 
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The integer value of the string
+ */
 int _atoi(char *s)
 {
 	int num = 0;
-	int t;
-	int count = 0;
-	int r;
-	int minus = 0;
 	int i = 0;
+	int sign = 1;
 
-	for (r = 0; s[r] != '\0'; r++)
+	if (s == NULL || *s == '\0')
+		return (0);
+
+	while (s[i] == ' ')
+		i++;
+
+	if (s[i] == '-' || s[i] == '+')
 	{
-		count++;
-	}
-	for (t = 0; t < count; t++)
-	{
-		if (s[t] == '-')
-		{
-			minus = 1;
-			i++;
-		}
-		else if (s[t] == '+')
-		{
-			i++;
-		}
-		else if (s[t] == ' ')
-		{
-			i++;
-		}
-		else if (s[t] >= 0 || s[t] <= 9)
-		{
-			num = num * 10 + (s[t] - 48);
-			i++;
-		}
-	}
-	if (minus == 1)
-	{
-		num = num * (-1);
+		if (s[i] == '-')
+			sign = -1;
+		i++;
 	}
 
-	return (num);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+
+		if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+
+		num = num * 10 + digit;
+		i++;
+	}
+
+	return (num * sign);
 }
